b2a77f4d6f40cb03809960f4831b1d086f559cc6
JangwonLim <jangwonlim12@gmail.com>
2024-07-19 00:44:37 -0400

    add a card with information

--- src/pages/profilePage/profile.jsx
+++ src/pages/profilePage/profile.jsx
@@ -1,25 +1,24 @@
 import React, { useEffect, useState } from "react";
 import './profile.css';
-// import Card from '../../components/Card/Card';
+import Card from '../../components/Card/Card';
 import { getMyInfo } from "../../service/getService";
 
 function Profile() {
   const [myInfo, setMyInfo] = useState(null);
 
-  const fetchMyInfo = async () => {
-    let result = await getMyInfo();
-    console.log(result);
-    setMyInfo(result);
-  }
-
   useEffect(() => {
+    const fetchMyInfo = async () => {
+      let result = await getMyInfo();
+      console.log("result: ", result.data);
+      setMyInfo(result.data);
+    }
+
     fetchMyInfo();
-    console.log("myInfo: ", myInfo);
-  }, [myInfo]);
+  }, []);
 
   return (
     <div>
-      {/* <Card data={myInfo}/> */}
+      <Card data={myInfo}/>
     </div>
   )
 }
