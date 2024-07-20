c60df6de668669ef7e2bab26050ef33dad3f6b3e
JangwonLim <jangwonlim12@gmail.com>
2024-07-20 16:21:05 -0400

    debug filters

--- src/service/getService.jsx
+++ src/service/getService.jsx
@@ -21,6 +21,7 @@ export const getInfo = async (page, size, direction, searchingWord, membership, 
     })
 
     if (response.data) {
+      console.log(response.data);
       return response.data;
     } else {
       console.log('No response data received');
@@ -70,38 +71,3 @@ export const getMyInfo = async () => {
     console.error(error);
   }
 }
-
-// const API_URL = 'https://gvzip.com';
-
-// export const getInfo = async (membership, campus, fields, page, size, direction) => {
-//   try {
-//     console.log(membership, campus, fields, page, size, direction);
-
-//     const params = new URLSearchParams({
-//       membership: membership,
-//       campus: campus,
-//       fields: fields,
-//       page: page,
-//       size: size,
-//       direction: direction
-//     }).toString();
-
-//     const response = await fetch(`${API_URL}/archive?${params}`, {
-//       method: 'GET',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       }
-//     });
-
-//     if (response.ok) {
-//       const data = await response.json();
-//       console.log(data);
-//       return data;
-//     } else {
-//       console.log('No response data received');
-//     }
-//   } catch (error) {
-//     console.log("Error occurred while fetching userinfo!");
-//     console.error(error);
-//   }
-// };
