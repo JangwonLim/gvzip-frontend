b7a62aecfa92c232625cd74f5e39b2dbbe55d7f5
JangwonLim <jangwonlim12@gmail.com>
2024-07-20 20:57:57 -0400

    test animation

--- src/components/BottomSheet/BottomSheet.jsx
+++ src/components/BottomSheet/BottomSheet.jsx
@@ -1,11 +1,65 @@
+// import React, { useRef, useEffect } from 'react';
+// import { useSpring, animated } from 'react-spring';
+// import { useDrag } from 'react-use-gesture';
+// import './BottomSheet.css';
+// import '../../pages/authenticate/SignUp/ProfileInfo.css';
+
+// function BottomSheet({Content, isBottomSheetOpen, closeBottomSheet, contentProps, onClickFilterOptions}) {
+//   const [{ y, height }, set] = useSpring(() => ({ y: 0, height: window.innerHeight }));
+//   const bottomSheetRef = useRef(null);
+//   const openHeight = window.innerHeight; // Bottom Sheet 열림 높이 (px)
+//   const maxDragUp = -30; // 위로 30px 드래그 가능
+//   const maxDragDown = openHeight * 0.5; // 아래로 50% 드래그 시 닫힘
+
+//   useEffect(() => {
+//     if (isBottomSheetOpen) {
+//       set({y: 0, height: window.innerHeight});
+//     }
+//   })
+
+//   const bind = useDrag(({ last, movement: [, my], memo = y.get() }) => {
+//     if (last) {
+//       if (my > maxDragDown) {
+//         closeBottomSheet();
+//       } else {
+//         set({ y: 0, height: openHeight });
+//       }
+//     } else {
+//       const newY = Math.max(my + memo, maxDragUp);
+//       set({ y: newY, height: openHeight - newY  });
+//     }
+//     return memo;
+//   }, { from: () => [0, y.get()] });
+
+//   return (
+//     <>
+//       {/* <button onClick={openBottomSheet}>open</button> */}
+//       {isBottomSheetOpen && <div className="bottom-sheet-overlay" onClick={closeBottomSheet} />}
+//       <animated.div
+//         className={`bottom-sheet ${isBottomSheetOpen ? 'open' : ''}`}
+//         style={{ transform: y.to(y => `translateY(${y}px)`), height }}
+//         {...bind()}
+//         ref={bottomSheetRef}
+//       >
+//         <div className="bottom-sheet-handle"></div>
+//         <div className="Profile--content-container">
+//           <Content contentProps={contentProps} onClickFilterOptions={onClickFilterOptions}/>
+//         </div>
+//       </animated.div>
+//     </>
+//   );
+// }
+
+// export default BottomSheet;
+
 import React, { useRef, useEffect } from 'react';
 import { useSpring, animated } from 'react-spring';
 import { useDrag } from 'react-use-gesture';
 import './BottomSheet.css';
 import '../../pages/authenticate/SignUp/ProfileInfo.css';
 
-function BottomSheet({Content, isBottomSheetOpen, closeBottomSheet, contentProps, onClickFilterOptions}) {
-  const [{ y, height }, set] = useSpring(() => ({ y: 0, height: window.innerHeight }));
+function BottomSheet({ Content, isBottomSheetOpen, closeBottomSheet, contentProps, onClickFilterOptions }) {
+  const [{ y, height }, set] = useSpring(() => ({ y: window.innerHeight, height: window.innerHeight }));
   const bottomSheetRef = useRef(null);
   const openHeight = window.innerHeight; // Bottom Sheet 열림 높이 (px)
   const maxDragUp = -30; // 위로 30px 드래그 가능
@@ -13,9 +67,11 @@ function BottomSheet({Content, isBottomSheetOpen, closeBottomSheet, contentProps
 
   useEffect(() => {
     if (isBottomSheetOpen) {
-      set({y: 0, height: window.innerHeight});
+      set({ y: 0, height: window.innerHeight });
+    } else {
+      set({ y: window.innerHeight });
     }
-  })
+  }, [isBottomSheetOpen, set]);
 
   const bind = useDrag(({ last, movement: [, my], memo = y.get() }) => {
     if (last) {
@@ -26,14 +82,13 @@ function BottomSheet({Content, isBottomSheetOpen, closeBottomSheet, contentProps
       }
     } else {
       const newY = Math.max(my + memo, maxDragUp);
-      set({ y: newY, height: openHeight - newY  });
+      set({ y: newY, height: openHeight - newY });
     }
     return memo;
   }, { from: () => [0, y.get()] });
 
   return (
     <>
-      {/* <button onClick={openBottomSheet}>open</button> */}
       {isBottomSheetOpen && <div className="bottom-sheet-overlay" onClick={closeBottomSheet} />}
       <animated.div
         className={`bottom-sheet ${isBottomSheetOpen ? 'open' : ''}`}
@@ -43,7 +98,7 @@ function BottomSheet({Content, isBottomSheetOpen, closeBottomSheet, contentProps
       >
         <div className="bottom-sheet-handle"></div>
         <div className="Profile--content-container">
-          <Content contentProps={contentProps} onClickFilterOptions={onClickFilterOptions}/>
+          <Content contentProps={contentProps} onClickFilterOptions={onClickFilterOptions} />
         </div>
       </animated.div>
     </>
