3e7e6302d39e3e438ebf7addb38dcd7e7fcb9ef6
JangwonLim <jangwonlim12@gmail.com>
2024-07-21 02:52:11 -0400

    test getCookie

--- src/utils/usefulFunctions.jsx
+++ src/utils/usefulFunctions.jsx
@@ -25,6 +25,7 @@ export const calculateGeneration = (graduationYear) => {
 
 export const getCookie = (name) => {
   const value = `; ${document.cookie}`;
+	console.log(document.cookie);
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(';').shift();
 }
