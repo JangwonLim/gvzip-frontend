35b0430e41de853770ff6042b3c2c8fe341ec9cd
JangwonLim <jangwonlim12@gmail.com>
2024-08-07 01:32:17 -0400

    change defaultValue to value in Gender component

--- src/components/SignUpComponents/Gender.jsx
+++ src/components/SignUpComponents/Gender.jsx
@@ -13,7 +13,7 @@ function Gender({formData, handleChange}) {
       <div className="Profile--button-container wide-gap">
         <button 
           className={"Profile--button" + (formData.sex === "여성" ? " selected" : "")}
-          defaultValue="여성"
+          value="여성"
           name="sex"
           onClick={handleChange}
         >
@@ -21,7 +21,7 @@ function Gender({formData, handleChange}) {
         </button>
         <button 
           className={"Profile--button" + (formData.sex === "남성" ? " selected" : "")}
-          defaultValue="남성"
+          value="남성"
           name="sex"
           onClick={handleChange}
         >
